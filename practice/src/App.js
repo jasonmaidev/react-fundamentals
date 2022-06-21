a18f9bbe7e3b317c8d2c26215dfef34fa19da79a
JFMai90 <jasonmaif@gmail.com>
2022-06-21 16:49:09 -0700

    update

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet />
+      <Message />
     </div>
   );
 }
